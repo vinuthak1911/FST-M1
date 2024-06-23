4f7ed93904ea16ed5a4b0fc802243669ffc7f1c4
vinuthak1911 <148183918+vinuthak1911@users.noreply.github.com>
2024-06-23 15:37:52 +0530

    Delete NotEnoughFundsException.java

--- NotEnoughFundsException.java
+++
@@ -1,7 +0,0 @@
-public class NotEnoughFundsException extends RuntimeException {
-
-    public NotEnoughFundsException(Integer amount, Integer balance) {
-        super("Attempted to withdraw " + amount + " with a balance of " + balance);
-    }
-
-}
